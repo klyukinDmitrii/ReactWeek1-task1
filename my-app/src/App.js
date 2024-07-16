40d0648d89b214156a973d8873becdc3e4a9f96e
Dmitrijklyukin <109663928+Dmitrijklyukin@users.noreply.github.com>
2024-07-16 10:54:56 +0300

    add  files without jsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 import { getDate } from './utils';
+import { createElement } from 'react';
 
 export const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{getDate()} year</p>
-			</header>
-		</div>
+	//Императивный
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', {
+				src: logo,
+				width: '500px',
+				className: 'App - logo',
+				alt: 'logo',
+			}),
+			createElement(
+				'p',
+				null,
+				'Edit',
+				createElement('code', null, 'src/App.js'),
+				'and save to reload',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+
+				'Learn React',
+			),
+			createElement('p', null, getDate()),
+		),
 	);
 };
--- my-app/src/utils.js
+++ my-app/src/utils.js
@@ -1,3 +1,4 @@
 export const getDate = () => {
+	// Императивный
 	return new Date().getFullYear();
 };
